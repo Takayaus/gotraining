350724e9faee19ee4c755e226da6d3f39c79b378
takayamori <morit617@gmail.com>
2020-05-07 14:49:05 +0900

    set StaticPages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  root 'staticpages#home'
+  get '/home', to:'staticpages#home'
+  get '/contact', to:'staticpages#contact'
+  get '/about', to:'staticpages#about'
+
 end
