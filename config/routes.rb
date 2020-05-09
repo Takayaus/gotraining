d9ae5b5f54181333f79c6e8b71763ab57cddde45
takayamori <morit617@gmail.com>
2020-05-09 10:58:40 +0900

    add follo, unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,16 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:index,:show]
-
+  resources :relationships,       only: [:create, :destroy]
   resources :posts
   root 'staticpages#home'
   get '/home', to:'staticpages#home'
   get '/contact', to:'staticpages#contact'
   get '/about', to:'staticpages#about'
 
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 end
