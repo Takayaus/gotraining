8245507044400a338bc57745f36a4d2362979516
takayamori <morit617@gmail.com>
2020-05-11 20:18:10 +0900

    adminのパスワード更新、エラー処理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!, except:[:home, :contact, :about]
+  before_action :authenticate_user!, only:[:index, :create, :destroy, :show, :folloing, :followers]
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,6 @@ User.create!(name:  "Example User",
                email: email,
                password:              password,
                password_confirmation: password)
+
 end
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
