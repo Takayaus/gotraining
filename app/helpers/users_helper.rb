6101acc91d64a7e89f4c7c81118a2178e0cbe980
takayamori <morit617@gmail.com>
2020-05-11 17:05:08 +0900

    画像表示index, show, postindex

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,11 @@ module UsersHelper
     def current_user?(user)
         user == current_user
      end
+
+     def gravatar_for(user, size: 80)
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+        image_tag(gravatar_url, alt: user.name, class: "gravatar")
+      end
+      
 end
--- db/schema.rb
+++ db/schema.rb
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 20200508133416) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
+    t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
