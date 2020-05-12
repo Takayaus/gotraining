9ed050cd738ba1caffe051546121c66e51c60f63
takayamori <morit617@gmail.com>
2020-05-12 10:18:33 +0900

    post.show追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,10 @@ class PostsController < ApplicationController
         @posts = @q.result(distinct: true)
     end
 
+    def show
+        @post = Post.find(params[:id])
+    end
+
     def create
         @post = current_user.posts.build(post_params)
         if @post.save
