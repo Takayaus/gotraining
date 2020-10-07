d68d68168b7f1877f1f45c7b97f05f23ba793613
takayamori <morit617@gmail.com>
2020-10-07 09:28:50 +0900

    controller 作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'announcements/index'
+
+  get 'announcements/show'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   
